8f8b0a3d14651dbf01ddabdc727cea6e025b50b8
Luis Mera <luismera92@gmail.com>
2023-03-17 15:04:35 -0500

    fix: Unnecessary escape character: \"

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -61,7 +61,7 @@ function ForgotPassword() {
                             required: "Este campo es requerido",
                             pattern: {
                               value:
-                                /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                '/^(([^<>()[]\\.,;:s@"]+(.[^<>()[]\\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/',
                               message:
                                 "¡Debe proporcionar una dirección de correo electrónico válida!",
                             },
